fcb5b4e9accd5c892a6b192b64f8b5f85cc6f01d
Wassim AZIRAR <wassim.azirar@gmail.com>
2018-03-13 02:08:23 +0100

    changed conf again

--- server.js
+++ server.js
@@ -16,11 +16,26 @@ api.addDatastore('coffees', datastore);
 app.use(cors());
 app.use('/', api);
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.configure(function() {
+  app.set(
+    'port',
+    process.env.OPENSHIFT_NODEJS_PORT ||
+      process.env.OPENSHIFT_INTERNAL_PORT ||
+      process.env.PORT ||
+      3000
+  );
+  app.set(
+    'ip',
+    process.env.OPENSHIFT_NODEJS_IP ||
+      process.env.OPENSHIFT_INTERNAL_IP ||
+      'localhost'
+  );
+});
 
-app.listen(server_port, server_ip_address, function() {
-  console.log('Listening on ' + server_ip_address + ', port ' + server_port);
+app.listen(app.get('port'), app.get('ip'), function() {
+  console.log(
+    'Express server listening on ' + app.get('ip') + ':' + app.get('port')
+  );
 });
 
 // app.listen(3000);
