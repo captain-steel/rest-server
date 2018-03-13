c621f0b6dca8fd2f12079e74ef6890e1387ec8db
Wassim AZIRAR <wassim.azirar@gmail.com>
2018-03-13 22:44:20 +0100

    revert back to initial server config

--- server.js
+++ server.js
@@ -16,22 +16,10 @@ api.addDatastore('coffees', datastore);
 app.use(cors());
 app.use('/', api);
 
-app.set(
-  'port',
-  process.env.OPENSHIFT_NODEJS_PORT ||
-    process.env.OPENSHIFT_INTERNAL_PORT ||
-    process.env.PORT ||
-    3000
-);
-app.set(
-  'ipaddr',
-  process.env.OPENSHIFT_NODEJS_IP ||
-    process.env.OPENSHIFT_INTERNAL_IP ||
-    'localhost'
-);
+app.set('port', process.env.PORT || 3000);
+app.set('ipaddr', 'localhost');
 
-// app.listen(app.get('port'), app.get('ipaddr'), function() {
-app.listen(app.get('port'), '120.130.0.0', function() {
+app.listen(app.get('port'), app.get('ipaddr'), function() {
   console.log(
     'Express server listening on ' + app.get('ipaddr') + ':' + app.get('port')
   );
