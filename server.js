97de1e45936c38f7444d9ef68e7445cce8195d2a
Wassim AZIRAR <wassim.azirar@gmail.com>
2018-03-13 02:21:24 +0100

    migrated from express 3 to 4

--- server.js
+++ server.js
@@ -16,28 +16,22 @@ api.addDatastore('coffees', datastore);
 app.use(cors());
 app.use('/', api);
 
-app.configure(function() {
-  app.set(
-    'port',
-    process.env.OPENSHIFT_NODEJS_PORT ||
-      process.env.OPENSHIFT_INTERNAL_PORT ||
-      process.env.PORT ||
-      3000
-  );
-  app.set(
-    'ip',
-    process.env.OPENSHIFT_NODEJS_IP ||
-      process.env.OPENSHIFT_INTERNAL_IP ||
-      'localhost'
-  );
-});
+app.set(
+  'port',
+  process.env.OPENSHIFT_NODEJS_PORT ||
+    process.env.OPENSHIFT_INTERNAL_PORT ||
+    process.env.PORT ||
+    3000
+);
+app.set(
+  'ipaddr',
+  process.env.OPENSHIFT_NODEJS_IP ||
+    process.env.OPENSHIFT_INTERNAL_IP ||
+    'localhost'
+);
 
-app.listen(app.get('port'), app.get('ip'), function() {
+app.listen(app.get('port'), app.get('ipaddr'), function() {
   console.log(
-    'Express server listening on ' + app.get('ip') + ':' + app.get('port')
+    'Express server listening on ' + app.get('ipaddr') + ':' + app.get('port')
   );
 });
-
-// app.listen(3000);
-
-// console.log('Server started successfully. http://localhost:3000/');
