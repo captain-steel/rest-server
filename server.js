bac5cb083e0e4943472a515da3e29904c7bf38c7
Wassim AZIRAR <wassim.azirar@gmail.com>
2018-03-13 02:02:22 +0100

    changed port and ip_address

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ api.addDatastore('coffees', datastore);
 app.use(cors());
 app.use('/', api);
 
-app.listen(3000);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-console.log('Server started successfully. http://localhost:3000/');
+app.listen(server_port, server_ip_address, function() {
+  console.log('Listening on ' + server_ip_address + ', port ' + server_port);
+});
+
+// app.listen(3000);
+
+// console.log('Server started successfully. http://localhost:3000/');
