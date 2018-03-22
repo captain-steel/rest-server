db7ecd6c690ec9397db3f3ea17efc9c55bf35ccc
Wassim AZIRAR <wassim.azirar@gmail.com>
2018-03-22 01:46:17 +0100

    added some express middlewares

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
-const express = require('express');
-const nedb = require('nedb');
-const rest = require('express-nedb-rest');
-const cors = require('cors');
+const compression  = require('compression');
+const cors         = require('cors');
+const emoji        = require('node-emoji');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const helmet       = require('helmet');
+const morgan       = require('morgan');
+const nedb         = require('nedb');
+const responseTime = require('response-time');
+const rest         = require('express-nedb-rest');
 
 const app = express();
 
-const datastore = new nedb({
-  filename: 'progressive_web_app.db',
+const coffeeDataStore = new nedb({
+  filename: 'api/coffees.db',
   autoload: true
 });
 
-const api = rest();
-api.addDatastore('coffees', datastore);
+const restAPI = rest();
+restAPI.addDatastore('coffees', coffeeDataStore);
 
+app.use(compression());
 app.use(cors());
-app.use('/', api);
+app.use(favicon(__dirname + '/images/favicon.ico'));
+app.use(helmet());
+app.use(morgan('common'));
+app.use(responseTime());
+app.use('/', restAPI);
 
 app.set('port', process.env.PORT || 3000);
 app.set('ipaddr', 'localhost');
 
 app.listen(app.get('port'), app.get('ipaddr'), function() {
-  console.log(
-    'Express server listening on ' + app.get('ipaddr') + ':' + app.get('port')
-  );
+  // https://raw.githubusercontent.com/omnidan/node-emoji/master/lib/emoji.json
+  console.log(emoji.get('hourglass'), emoji.get('zap'), emoji.get('cat'), 'The server is running @ ' + app.get('ipaddr') + ':' + app.get('port'));
 });
