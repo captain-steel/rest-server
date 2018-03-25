c152d7f28dd9052d94251a77812ca5ca5ad3c275
Wassim AZIRAR <wassim.azirar@gmail.com>
2018-03-25 03:56:14 +0200

    add https support

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const cors         = require('cors');
 const emoji        = require('node-emoji');
 const express      = require('express');
 const favicon      = require('serve-favicon');
+const fs           = require('fs');
 const helmet       = require('helmet');
+const https        = require('https');
 const morgan       = require('morgan');
 const nedb         = require('nedb');
 const responseTime = require('response-time');
@@ -15,6 +17,11 @@ const coffeeDataStore = new nedb({
   filename: 'api/coffees.db',
   autoload: true
 });
+
+const options = {
+  cert: fs.readFileSync('/etc/letsencrypt/live/angular-pwa.westeurope.cloudapp.azure.com/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/angular-pwa.westeurope.cloudapp.azure.com/privkey.pem')
+};
 
 const restAPI = rest();
 restAPI.addDatastore('coffees', coffeeDataStore);
@@ -28,7 +35,7 @@ app.use(responseTime());
 app.use('/', restAPI);
 
 app.set('port', process.env.PORT || 3000);
-app.set('ipaddr', 'localhost');
+app.set('ipaddr', '0.0.0.0');
 
 app.listen(app.get('port'), app.get('ipaddr'), function() {
   // https://raw.githubusercontent.com/omnidan/node-emoji/master/lib/emoji.json
@@ -39,3 +46,4 @@ app.listen(app.get('port'), app.get('ipaddr'), function() {
     'The server is running @ ' + app.get('ipaddr') + ':' + app.get('port')
   );
 });
+https.createServer(options, app).listen(8443);
