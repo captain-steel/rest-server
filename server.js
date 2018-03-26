59f9e0be878646e6177f40967b9c91b1f30d1b20
Wassim AZIRAR <wassim.azirar@gmail.com>
2018-03-27 00:23:59 +0200

    limit each IP to 50 requests per 15 minutes

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const options = {
 
 var limiter = new rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 50, // limit each IP to 100 requests per windowMs
+  max: 50, // limit each IP to 50 requests per windowMs
   delayMs: 0 // disable delaying - full speed until the max limit is reached
 });
 
